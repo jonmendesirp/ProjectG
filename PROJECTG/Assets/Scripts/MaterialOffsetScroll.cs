49b91a8e58dfc9b9f33771a3155e8c324d61edb0
jonmendesirp <jon.mendes.irp@gmail.com>
2022-06-07 12:21:32 +0100

    paredes_corredor

---
+++ PROJECTG/Assets/Scripts/MaterialOffsetScroll.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialOffsetScroll : MonoBehaviour
+{
+    [SerializeField] private float speed = 0.5f;
+
+    void Update()
+    {
+        float offset = Time.time * speed;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(0, offset);
+        
+    }
+}
