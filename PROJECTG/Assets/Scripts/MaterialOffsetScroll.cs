56e8656454297f8381c5caa250d80a1cc7c9f6d1
jonmendesirp <jon.mendes.irp@gmail.com>
2022-06-07 15:15:55 +0100

    Level02Corridor

--- PROJECTG/Assets/Scripts/MaterialOffsetScroll.cs
+++ PROJECTG/Assets/Scripts/MaterialOffsetScroll.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MaterialOffsetScroll : MonoBehaviour
 {
-    [SerializeField] private float speed = 0.5f;
+    [SerializeField] private float speed = -0.3f;
 
     void Update()
     {
