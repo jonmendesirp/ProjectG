6dba7c7ac7af86ff807c4f409548fc13b7e8e69e
jonmendesirp <jon.mendes.irp@gmail.com>
2022-06-07 15:25:57 +0100

    inimigos

--- PROJECTG/Assets/Scripts/Enemy.cs
+++ PROJECTG/Assets/Scripts/Enemy.cs
@@ -5,24 +5,33 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    
-    NavMeshAgent agent; 
+
+    NavMeshAgent agent;
     Transform target;
 
-    public bool playerIsOnArea = false; 
+    public bool playerIsOnArea = false;
+    Vector3 randomLook;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
+        randomLook = new Vector3(Random.Range(0, 200), Random.Range(0, 200), Random.Range(0, 200));
     }
 
 
     void Update()
     {
-        if(playerIsOnArea) transform.LookAt(target);
-        else transform.LookAt(new Vector3(Random.Range(0, 200), Random.Range(0, 200), Random.Range(0, 200)));
+        if (playerIsOnArea)
+        {
+            transform.LookAt(target);
+        }
+        else
+        {
+            transform.LookAt(randomLook);
+        }
+
         agent.destination = target.position;
-        
+
     }
 }
