6a17ce571dfeb12e7e4c67ed223d4bbb44bc7599
OwAbbie <americangirl13579@gmail.com>
2022-06-13 12:10:53 +0100

    huge

---
+++ PROJECTG/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // chamar o Scene Management pra poder passar pra Scene ao clicar no play
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame() //chamou-se PlayGame numa de começar o jogo, mas it's up to you
+    {
+        SceneManager.LoadScene("NivelJogo"); //meter entre as aspas o nome da Scene onde o jogo vai começar
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
